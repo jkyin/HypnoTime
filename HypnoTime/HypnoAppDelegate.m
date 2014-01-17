1544b514d54d95e03f64c71d7ca23177e5e00122
Yin <yinxiaoyu888@gmail.com>
2014-01-17 20:04:59 +0800

    创建 TimeViewController 显示另一个 TImeView 视图。

--- HypnoTime/HypnoAppDelegate.m
+++ HypnoTime/HypnoAppDelegate.m
@@ -14,20 +14,19 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    
-//    // 将新建的 HypnosisViewController 实例赋给 UIWindow 实例的 rootViewController 属性
+//    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+//    
+////    // 将新建的 HypnosisViewController 实例赋给 UIWindow 实例的 rootViewController 属性
 //    HypnosisViewController *hvc = [[HypnosisViewController alloc] init];
-//    [[self window] setRootViewController:hvc];
-    TimeViewController *tvc = [[TimeViewController alloc] init];
-    [[self window] setRootViewController:tvc];
-    
-    
-    
-    
-    // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
-    [self.window makeKeyAndVisible];
+////    [[self window] setRootViewController:hvc];
+//
+//    TimeViewController *tvc = [[TimeViewController alloc] init];
+//    [[self window] setRootViewController:tvc];
+//    [[self window] addSubview:[tvc timeView]];
+//    
+//    // Override point for customization after application launch.
+//    self.window.backgroundColor = [UIColor whiteColor];
+//    [self.window makeKeyAndVisible];
     return YES;
 }
 
