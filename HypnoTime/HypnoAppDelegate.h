7cd4a88ae0b4bbd0268b8eec19ffd87d6130742d
Yin <yinxiaoyu888@gmail.com>
2014-01-17 14:16:40 +0800

    Initial Commit

---
+++ HypnoTime/HypnoAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HypnoAppDelegate.h
+//  HypnoTime
+//
+//  Created by Yin on 14-1-17.
+//  Copyright (c) 2014年 Jack Yin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnoAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HypnoTime/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HypnoTime
+//
+//  Created by Yin on 14-1-17.
+//  Copyright (c) 2014年 Jack Yin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HypnoAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HypnoAppDelegate class]));
+    }
+}
